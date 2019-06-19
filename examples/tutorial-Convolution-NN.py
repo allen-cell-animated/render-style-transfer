0c419ae168aa3ec1342a97912643da5206a5087c
Dan Toloudis <danielt@alleninstitute.org>
2019-06-19 08:46:02 -0700

    more commenting

--- examples/tutorial-Convolution-NN.py
+++ examples/tutorial-Convolution-NN.py
@@ -9,7 +9,7 @@ import torch.optim as optim
 import matplotlib.pyplot as plt
 import numpy as np
 
-# transforms an image to a number
+# transforms an image to a Tensor, with a normalization operation
 transform = transforms.Compose(
     [transforms.ToTensor(),
      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
@@ -23,7 +23,9 @@ trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
 
 #Data loader. Combines a dataset and a sampler, and provides single- or multi-process iterators over the dataset.
 
-# takes the trainset we defined, loads 4 (default 1) at a time, reshuffles the data every epoch
+# takes the trainset we defined, loads 4 (default 1) at a time, 
+# shuffle=True reshuffles the data every epoch
+# for shuffle, an epoch is defined as one full iteration through the DataLoader
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
                                           shuffle=True, num_workers=2)
 
@@ -68,21 +70,31 @@ class Net(nn.Module):
         super(Net, self).__init__()
         #  nn.Conv2d: Applies a 2D convolution over an input signal composed of several input planes.
         # args: in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True, padding_mode='zeros'
+        # This results in 2 "parameters": 6 learned kernels of size 5x5 per input channel (3) plus 6 learned scalar bias terms
         self.conv1 = nn.Conv2d(3, 6, 5) 
         # Applies a 2D max pooling over an input signal composed of several input planes.
         # args: (kernel_size, stride=None, padding=0, dilation=1, return_indices=False, ceil_mode=False)
+        # This results in 2x2 downsampling with a "max" filter
         self.pool = nn.MaxPool2d(2, 2)
+        # This results in 2 "parameters": 16 learned kernels of size 5x5 per input channel (6) plus 16 learned scalar bias terms
         self.conv2 = nn.Conv2d(6, 16, 5)
         # Applies a linear transformation to the incoming data: y = xA^T + b
         # args: (in_features, out_features, bias=True)
+        # This results in 2 "parameters": 120 learned linear weight vectors of length 16*5*5, plus 120 learned scalar bias terms
         self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        # This results in 2 "parameters": 84 learned linear weight vectors of length 120, plus 84 learned scalar bias terms
         self.fc2 = nn.Linear(120, 84)
+        # This results in 2 "parameters": 10 learned linear weight vectors of length 84, plus 10 learned scalar bias terms
         self.fc3 = nn.Linear(84, 10)
 
     # running data through the network
     def forward(self, x):
+        # relu seems to basically clip the min at 0
         x = self.pool(F.relu(self.conv1(x)))
+        # relu seems to basically clip the min at 0
         x = self.pool(F.relu(self.conv2(x)))
+        # reshape tensor x to have its second dimension be of size 16*5*5,
+        # (to fit into the fc1 ?) 
         x = x.view(-1, 16 * 5 * 5)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
@@ -111,11 +123,13 @@ for epoch in range(1):  # loop over the dataset multiple times
         # zero the parameter gradients
         optimizer.zero_grad()
 
-        # forward + backward + optimize
+        # run batch forward
         outputs = net(inputs)
+        # calculate loss function
         loss = criterion(outputs, labels)
-        # TODO: look into these more
+        # calculate (accumulate) gradients for whole network
         loss.backward()
+        # perform parameter updates based on current gradients
         optimizer.step()
 
         # print statistics
@@ -129,7 +143,7 @@ print('Finished Training')
 
 
 dataiter = iter(testloader)
-# grab four images again but from data it hasn't seen before
+# grab batch of four images again but from data it hasn't seen before
 images, labels = dataiter.next()
 
 # print images
@@ -142,9 +156,9 @@ print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
 # higher number means more confident
 outputs = net(images)
 # print(outputs)
-# outputs are a tensor, we want the one it thinks is most likely 
-# we are getting the index of the max value so we can get the name of the class from the classes array
-#torch.max returns (max_value, index)
+# outputs are tensors, but we want the one it thinks is most likely 
+# torch.max returns (max_value, index)
+# therefore "predicted" will hold the index of the max value so we can get the name of the class from the classes array
 _, predicted = torch.max(outputs, 1)
 
 print('Predicted: ', ' '.join('%5s' % classes[predicted[j]]
@@ -152,9 +166,8 @@ print('Predicted: ', ' '.join('%5s' % classes[predicted[j]]
 
 correct = 0
 total = 0
-# just in prediction mode, not learning mode
 # calc percentage calc labels match the real labels in the testing set
-with torch.no_grad():
+with torch.no_grad():  # just in prediction mode, not learning mode
     for data in testloader:
         images, labels = data
         outputs = net(images)
